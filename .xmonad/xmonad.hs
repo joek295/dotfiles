9eea06c9bd23a95079df8f7a2e3b2cd086717483
Joe Kitchen <joe@athene.(none)>
2013-04-22 11:34:42 +0100

    XMonad modified:   .conkyrc new file:   .xmonad/xmoconky modified:   .xmonad/xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+import XMonad.Layout.Circle
+import XMonad.Layout.ResizableTile
 
 myTerminal = "/usr/bin/urxvt"
 
@@ -10,15 +12,16 @@ myManageHook = composeAll
   ]
 
 myLayout = avoidStruts (
+  -- ResizableTall: 1/2 vertical 1 window/ other windows split
   ResizableTall 1 (3/100) (1/2) [] |||
-  noBorders Full |||
+  Full |||
   Circle )
 
 main = do
   xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook myManageHook 
     , layoutHook = myLayout
-    , startupHook = spawn "conky"
+    , startupHook = spawn "conky -c ~/.xmonad/xmoconky"
     , terminal = myTerminal
     } `additionalKeys`
     -- list of additional custom keybindings:
@@ -31,7 +34,7 @@ main = do
     , ((mod1Mask .|. shiftMask, xK_p), spawn "dmenufm")
     , ((mod1Mask, xK_r), restart "xmonad" True)
     -- Win-[1..3] and Win-S-[1..3] flip to and move client to monitor 1-3
-    , ((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_1, xK_2, xK_3] [0..]
-      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+--    , ((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+--      | (key, sc) <- zip [xK_1, xK_2, xK_3] [0..]
+--      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
