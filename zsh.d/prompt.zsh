c981819a1244df632ebc1275dd27888916965aed
Joe Kitchen <joe@grill>
2015-07-06 21:27:40 +0100

    Cleaning up and tweaking prompts

--- zsh.d/prompt.zsh
+++ zsh.d/prompt.zsh
@@ -7,7 +7,7 @@ RED=%{$fg[red]%}
 YELLOW=%{$fg[yellow]%}
 
 function git_branch {
-    git branch >/dev/null 2>/dev/null && echo "($(git branch))"
+    git branch >/dev/null 2>/dev/null && echo -n "(" && echo "$(git branch) )" | cut -c 2-
 }
 
 function git_status {
@@ -38,7 +38,7 @@ INSERT_PROMPT="$RED INSERT$COLOR_OFF"
 # RPS1/2 should tell us the mode when in vi-mode
 function zle-line-init zle-keymap-select {
     GIT_PROMPT="$(git_status)"
-    RPS1="$GIT_PROMPT [ ${${KEYMAP/vicmd/$NORMAL_PROMPT}/(main|viins)/$INSERT_PROMPT} ]"
+    RPS1="$GIT_PROMPT [${${KEYMAP/vicmd/$NORMAL_PROMPT}/(main|viins)/$INSERT_PROMPT} ]"
     RPS2=$RPS1
     zle reset-prompt
 }
