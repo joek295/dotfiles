afe6a5821119f08dc7d3d91c78d7987ff483990d
Joe Kitchen <joe@grill>
2015-07-06 19:46:55 +0100

    zsh prompt now indicates git branch if applicable

--- zsh.d/prompt.zsh
+++ zsh.d/prompt.zsh
@@ -1,5 +1,11 @@
-PS1="%{$fg[blue]%}%n%{$fg[default]%}@%m:%{$fg[green]%}%~
-%{%(#~$fg[red]~$fg[default])%}%#%{$fg[default]%} "
+# -*- sh -*-
+
+function git_branch {
+    git branch >/dev/null 2>/dev/null && echo "($(git branch))"
+}
+
+PS1='%{$fg[blue]%}%n%{$fg[default]%}@%m:%{$fg[green]%}%~ %{$fg[yellow]%}$(git_branch)
+%{%(#~$fg[red]~$fg[default])%}%#%{$fg[default]%} '
 
 # RPS1/2 should tell us the mode when in vi-mode
 function zle-line-init zle-keymap-select {
