3e629cf9bd1581b4f9cd167151098da86c4ec92c
Joe Kitchen <joe@grill>
2015-07-06 20:05:34 +0100

    Cleaned up for readability
    While the colouring code for zsh is much more readable than bash's
    attempt (though that's a low bar!), it's still much cleaner to replace
    all of the boilerplate with $BLUE etc...
    
    Hacked up a little bit of script to show the prompt character as $ for
    normal users and # as root (with the # coloured red) to match practice
    in other shells...

--- zsh.d/prompt.zsh
+++ zsh.d/prompt.zsh
@@ -4,8 +4,22 @@ function git_branch {
     git branch >/dev/null 2>/dev/null && echo "($(git branch))"
 }
 
-PS1='%{$fg[blue]%}%n%{$fg[default]%}@%m:%{$fg[green]%}%~ %{$fg[yellow]%}$(git_branch)
-%{%(#~$fg[red]~$fg[default])%}%#%{$fg[default]%} '
+function prompt_char {
+    if [[ $(whoami) == "root" ]]; then
+        echo "$RED#$COLOR_OFF " && return
+    else
+        echo "$COLOR_OFF$ " && return
+    fi
+}
+
+COLOR_OFF=%{$fg[default]%}
+BLUE=%{$fg[blue]%}
+RED=%{$fg[red]%}
+YELLOW=%{$fg[yellow]%}
+GREEN=%{$fg[green]%}
+
+PS1='$BLUE%n$COLOR_OFF@%m:$GREEN%~ $YELLOW$(git_branch)
+$(prompt_char)'
 
 # RPS1/2 should tell us the mode when in vi-mode
 function zle-line-init zle-keymap-select {
