37ff242cb4651bdca7368aefde6562611301f7ad
Joe Kitchen <joe@grill>
2015-07-10 20:07:57 +0100

    Stripped out completion settings from .zshrc
    Now in completions.zsh...

---
+++ zsh.d/completions.zsh
@@ -0,0 +1,16 @@
+setopt AUTO_LIST        # tabbing on an ambiguously incomplete command lists possibilities
+setopt MENU_COMPLETE    # tabbing on an ambiguously incomplete command selects first possibility
+setopt COMPLETE_IN_WORD # tabbing in the middle of a word will try to complete that word
+setopt COMPLETE_ALIASES
+setopt AUTO_PARAM_SLASH 
+
+zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
+zstyle ':completion:*:expand:*' tag-order all-expansions
+# zsh better completion of kill command:
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+# completion for my functions...
+zstyle ':completion:*:*:extract:*' file-patterns '*.tar *.*bz2 *.*gz *.xz *.rar *.cbr *.zip *.cbz *.Z *.7z *(-/)'
+zstyle ':completion:*:*:pdf:*' file-patterns '*.tex *(-/)'
+
+autoload -U compinit && compinit
+compdef mtoc=man
